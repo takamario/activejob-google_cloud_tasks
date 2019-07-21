0f723a48cf8595831433fa3d51e6c91c69b10702
Shoei Takamaru <shoei.takamaru@gmail.com>
2019-07-21 18:37:03 +0900

    [Monkey patch] Support http_request target (#1)

--- lib/activejob/google_cloud_tasks/config.rb
+++ lib/activejob/google_cloud_tasks/config.rb
@@ -1,15 +1,35 @@
 module Activejob
   module GoogleCloudTasks
     class Config
-      DEFAULT_PATH = '/activejobs'
+      DEFAULT_PATH = '/activejobs'.freeze
+      DEFAULT_TARGET = :app_engine
 
-      def self.path
-        @path.presence || DEFAULT_PATH
-      end
+      class << self
+        def path
+          @path.presence || DEFAULT_PATH
+        end
 
-      def self.path=(path)
-        raise "path can't be blank" unless path.present?
-        @path = path
+        def path=(path)
+          raise "path can't be blank" if path.blank?
+
+          @path = path
+        end
+
+        def target
+          @target.presence || DEFAULT_TARGET
+        end
+
+        def target=(target)
+          @target = target.to_sym
+        end
+
+        def http_request?
+          @target == :http
+        end
+
+        def app_engine_http_request?
+          @target == DEFAULT_TARGET
+        end
       end
     end
   end
